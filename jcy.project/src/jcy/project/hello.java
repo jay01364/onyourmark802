7771b818663eafb76b1da17fc29e98f72c0e0b13
장두연 <@DESKTOP-A7OLLDL>
2023-09-11 22:16:08 +0900

    hi yes

---
+++ jcy.project/src/jcy/project/hello.java
@@ -0,0 +1,11 @@
+package jcy.project;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello java! I'm chaeyeon.");
+		System.out.println("안녕하세요.");
+
+	}
+
+}
